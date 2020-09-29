438bcdb3e35ae7313f0f5bc2601e381cd7d1aef2
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2020-09-29 13:09:25 +0200

    made a function out of it

--- cleaning.R
+++ cleaning.R
@@ -1,5 +1,5 @@
-library(tidyverse)
-library(fuzzyjoin)
+# library(tidyverse)
+# library(fuzzyjoin)
 
 # measurement <- 120 #the length of the measurement taken on the field in seconds
 # startcrop <- 20 #how much to crop at the beginning of the measurement in seconds
@@ -27,27 +27,33 @@ library(fuzzyjoin)
 
 #extract from fluxes the data that are between "starting_time" and +2mn, associate Site, type, and replicate to each CO2 entry
 #https://community.rstudio.com/t/tidy-way-to-range-join-tables-on-an-interval-of-dates/7881/2
-co2conc <- fuzzy_left_join( #there is probably a better way to do that than fuzzy join, I need to find out. Fuzzy join is using too much memory
-  fluxes, field_data,
-  by = c(
-    "Datetime" = "Start",
-    "Datetime" = "End"
-  ),
-  match_fun = list(`>=`, `<=`)
-) %>% 
-  drop_na(Start) %>% 
-  mutate(ID = group_indices(., Date, Site, Type, Replicate)) %>% #gives a unique ID to each flux
-  select(Datetime, CO2, PAR, Temp_air, Site, Type, Replicate, ID, Remarks, Date)
+
+cleaning <- function(fluxes, field_data){
+  co2conc <- fuzzy_left_join( #there is probably a better way to do that than fuzzy join, I need to find out. Fuzzy join is using too much memory
+    fluxes, field_data,
+    by = c(
+      "Datetime" = "Start",
+      "Datetime" = "End"
+    ),
+    match_fun = list(`>=`, `<=`)
+  ) %>% 
+    drop_na(Start) %>% 
+    mutate(ID = group_indices(., Date, Site, Type, Replicate)) %>% #gives a unique ID to each flux
+    select(Datetime, CO2, PAR, Temp_air, Site, Type, Replicate, ID, Remarks, Date)
+  return(co2conc)
+}
+
+
 
 # write_csv(co2conc_bio102, "co2conc_bio102.csv")
 # 
 # #graph CO2 fluxes to visually check the data
 # 
 # 
-ggplot(co2conc, aes(x=Datetime, y=CO2)) +
-  # geom_point(size=0.005) +
-  geom_line(size = 0.1, aes(group = ID)) +
-  coord_fixed(ratio = 10) +
-  scale_x_datetime(date_breaks = "30 min") +
-  # geom_line(size=0.05)
-  ggsave("bio102.png", height = 20, width = 40, units = "cm")
+# ggplot(co2conc, aes(x=Datetime, y=CO2)) +
+#   # geom_point(size=0.005) +
+#   geom_line(size = 0.1, aes(group = ID)) +
+#   coord_fixed(ratio = 10) +
+#   scale_x_datetime(date_breaks = "30 min") +
+#   # geom_line(size=0.05)
+#   ggsave("bio102.png", height = 20, width = 40, units = "cm")
